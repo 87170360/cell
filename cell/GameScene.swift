59d98bc528d6a4ece190cb7cccffee9b58aadba3
87170360 <87170360@qq.com>
2016-02-15 21:47:30 +0800

    add resource

--- cell/GameScene.swift
+++ cell/GameScene.swift
@@ -9,8 +9,12 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    let lifeCell = SKSpriteNode(imageNamed: "grass")
+    
     override func didMoveToView(view: SKView) {
         backgroundColor = SKColor.blackColor()
+        lifeCell.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        addChild(lifeCell)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
