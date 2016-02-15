e16e8624e8f595e99e31c5638851a14b5c87a252
87170360 <87170360@qq.com>
2016-02-15 22:41:49 +0800

    fill screen

--- cell/GameScene.swift
+++ cell/GameScene.swift
@@ -9,12 +9,38 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    let lifeCell = SKSpriteNode(imageNamed: "grass")
+    let water = SKTexture(imageNamed: "water")
+    let grass = SKTexture(imageNamed: "grass")
+    
+    var grid:[SKSpriteNode] = []
+    
+    override init(size: CGSize) {
+        let nodeSize = Int(water.size().width)
+        let widthNum = Int(Int(size.width) / nodeSize)
+        let heightNum = Int(Int(size.height) / nodeSize)
+        
+        for i in 1...widthNum {
+            for j in 1...heightNum {
+                let node = SKSpriteNode(imageNamed: "water")
+                node.position = CGPoint(x: -nodeSize / 2 + i * nodeSize, y: -nodeSize / 2 + j * nodeSize)
+                grid.append(node)
+            }
+        }
+        
+        super.init(size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("xx")
+    }
     
     override func didMoveToView(view: SKView) {
         backgroundColor = SKColor.blackColor()
-        lifeCell.position = CGPoint(x: size.width / 2, y: size.height / 2)
-        addChild(lifeCell)
+        
+        for item in grid {
+            addChild(item)
+        }
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
